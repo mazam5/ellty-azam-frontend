9b25313fedce09015d0da6fbe613991942398b39
mazam5 <mohammedazam651@gmail.com>
2025-04-02 11:36:56 +0530

    fixed: macro interactivity

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,16 +1,18 @@
 import Check from "../assets/icons/Check";
 
-const Checkbox = ({ isChecked, handleChange, isHovered }) => {
+const Checkbox = ({ isChecked, handleChange, isHovered, isFocused }) => {
   return (
     <span
-      className={`flex h-6 w-6 items-center justify-center rounded-md border border-[#CDCDCD] active:bg-[#5087F8] active:outline-[3px] active:outline-[#A3C3FA] ${isChecked ? "bg-[#2469F6]" : ""} ${isChecked && isHovered ? "bg-[#5087F8]" : ""}`}
+      className={`flex h-6 w-6 items-center justify-center rounded-md border border-[#CDCDCD] ${
+        isChecked ? "bg-[#2469F6]" : ""
+      } ${isChecked && isHovered ? "bg-[#5087F8]" : ""} ${isFocused ? "border-[#A3C3FA] outline-[3px] outline-[#A3C3FA]" : ""} `}
       onClick={(e) => {
         e.stopPropagation();
         handleChange();
       }}
     >
-      {isHovered ? (
-        <Check fill={isChecked ? "white" : "#E3E3E3"} />
+      {isHovered || isFocused ? (
+        <Check fill={isFocused ? "black" : isChecked ? "white" : "#E3E3E3"} />
       ) : (
         isChecked && <Check fill="white" />
       )}
--- src/components/PageItem.jsx
+++ src/components/PageItem.jsx
@@ -3,19 +3,26 @@ import Checkbox from "./Checkbox";
 
 const PageItem = ({ id, title, isChecked, onSelect }) => {
   const [isHovered, setIsHovered] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
 
   return (
     <div
       className="flex cursor-pointer items-center justify-between rounded-md p-2"
       onClick={() => onSelect(id)}
       onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
+      onMouseLeave={() => {
+        setIsHovered(false);
+        setIsFocused(false);
+      }}
+      onMouseDown={() => setIsFocused(true)}
+      onMouseUp={() => setIsFocused(false)}
     >
       <label className="text-sm">{title}</label>
       <Checkbox
         isChecked={isChecked}
         handleChange={() => onSelect(id)}
         isHovered={isHovered}
+        isFocused={isFocused}
       />
     </div>
   );
